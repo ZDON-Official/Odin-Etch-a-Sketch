6a436a2ca900439556bd28a007ad6154a6be2572
Zohaib <zrana1214@gmail.com>
2023-08-18 23:13:34 -0400

    added 16x16 grid

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const board = document.getElementById("board")
+const board = document.getElementById("board");
 
 // loops 16 times
-for(var i = 0; i < 16; i++){
-    board.innerHTML = `<div class = "innerBoard"></div>`
+for (var i = 0; i < 16; i++) {
+  var row = document.createElement("div");
+  board.appendChild(row);
+  row.className = "row";
+
+  for (var j = 0; j < 16; j++) {
+    var column = document.createElement("div");
+    row.appendChild(column);
+    column.className = "column";
+  }
 }
+
+// const rows = document.querySelectorAll(".rows");
+// // for columns
+// for (var i = 0; i < 16; i++) {
+//   console.log("inside column");
+//   rows.innerHTML = "1";
+// }
