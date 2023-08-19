49294cdff4ff37ab38e8f30ce6504b75a437f003
Zohaib <zrana1214@gmail.com>
2023-08-19 01:59:09 -0400

    added function for resizing and changing paint

--- script.js
+++ script.js
@@ -2,32 +2,44 @@ const board = document.getElementById("board");
 
 let paint = "violet";
 let size = 32;
-var board_width = 600;
+const board_width = 600;
 
 var col_size = board_width / size;
 
-console.log(`col_size: ${col_size}`);
+const col_hover = document.getElementsByClassName("column");
 
-// loops 16 times
-for (var i = 0; i < size; i++) {
-  var row = document.createElement("div");
-  board.appendChild(row);
-  row.className = "row";
-  row.style.height = col_size + "px";
+draw_board();
 
-  for (var j = 0; j < size; j++) {
-    var column = document.createElement("div");
-    row.appendChild(column);
-    column.className = "column";
-    column.style.height = col_size + "px";
-    column.style.width = col_size + "px";
+// draws the sketch board
+function draw_board() {
+  for (var i = 0; i < size; i++) {
+    var row = document.createElement("div");
+    board.appendChild(row);
+    row.className = "row";
+    row.style.height = col_size + "px";
+
+    for (var j = 0; j < size; j++) {
+      var column = document.createElement("div");
+      row.appendChild(column);
+      column.className = "column";
+      column.style.height = col_size + "px";
+      column.style.width = col_size + "px";
+    }
   }
 }
 
-const col_hover = document.getElementsByClassName("column");
+// change the size of the grid
+function resize(new_size) {
+  size = new_size;
 
-// TODO: add here
-function draw_board() {}
+  console.log(`new size is ${size}`);
+  console.log(`new col_size is ${col_size}`);
+}
+
+// TODO: add code to change the paint color
+function change_color(){
+    // ! add code here
+}
 
 Array.from(col_hover).forEach(function (element) {
   element.addEventListener("mouseover", function () {
