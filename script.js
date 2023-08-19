19c941e7d59e326bae3a5849d4ed2d7a45e389e5
Zohaib <zrana1214@gmail.com>
2023-08-19 01:45:45 -0400

    dynamic grid added

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const board = document.getElementById("board");
 
 let paint = "violet";
+let size = 32;
+var board_width = 600;
+
+var col_size = board_width / size;
+
+console.log(`col_size: ${col_size}`);
 
 // loops 16 times
-for (var i = 0; i < 16; i++) {
+for (var i = 0; i < size; i++) {
   var row = document.createElement("div");
   board.appendChild(row);
   row.className = "row";
+  row.style.height = col_size + "px";
 
-  for (var j = 0; j < 16; j++) {
+  for (var j = 0; j < size; j++) {
     var column = document.createElement("div");
     row.appendChild(column);
     column.className = "column";
+    column.style.height = col_size + "px";
+    column.style.width = col_size + "px";
   }
 }
 
 const col_hover = document.getElementsByClassName("column");
+
+// TODO: add here
+function draw_board() {}
 
 Array.from(col_hover).forEach(function (element) {
   element.addEventListener("mouseover", function () {
